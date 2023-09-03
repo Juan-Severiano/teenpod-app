91c2a447c6feda8679967745fd5cba28637bedb4
juan <juansoussev@gmail.com>
2023-09-03 19:34:18 -0300

    Login e Cadastro concluido

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,32 +1,41 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, TouchableOpacity, TextInput, Linking, FlatList } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, TextInput, Linking, FlatList, ImageBackground, Alert } from 'react-native'
 import Ionicons from '@expo/vector-icons/Ionicons'
 import axios from 'axios'
-import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default class Login extends Component {
   state = {
-    email: '',
+    username: '',
     password: '',
+    firstName: '',
+    lastName: '',
+    confirmPassword: '',
+    stageNew: false,
     showError: false,
     errors: []
   }
 
+  signinOrSignup = () => {
+    if (this.state.stageNew) {
+      Alert.alert('Sucesso!', 'Criar conta')
+      this.register()
+    } else {
+      Alert.alert('Sucesso!', 'Logar')
+      this.login()
+    }
+    console.log(this.state)
+  }
+
   login = async () => {
-    console.log('asdasdasdasd');
+    console.log('iniciando o login');
 
     const headers = {
       'Content-Type': 'application/json',
     };
 
-    // const data = {
-    //   username: this.state.email,
-    //   password: this.state.password,
-    // };
-
     const data = {
-      username: 'admin',
-      password: '@dmin12345678',
+      username: this.state.username,
+      password: this.state.password,
     };
 
     try {
@@ -55,19 +64,89 @@ export default class Login extends Component {
     }
   }
 
+  register = async () => {
+    console.log('iniciando o registro');
+
+    const headers = {
+      'Content-Type': 'application/json',
+    };
+
+    const data = {
+      username: this.state.username,
+      first_name: this.state.firstName,
+      last_name: this.state.lastName,
+      password: this.state.password,
+      confirm_password: this.state.confirmPassword
+    };
+
+    try {
+      const response = await axios.post(
+        'https://teenpod.pythonanywhere.com/api/users/',
+        data,
+        { headers }
+      );
+
+      console.log('STATUS Register', response.status);
+      console.log(response.data);
+
+      if (response.status === 201) {
+        this.setState({
+          username: '',
+          password: '',
+          firstName: '',
+          lastName: '',
+          confirmPassword: '',
+          stageNew: false,
+          showError: false,
+          errors: []
+        })
+      } else if (response.status === 400 || response.status === 401) {
+        this.setState({ showError: true });
+        const errors = [];
+        response.data.username ? errors.push(`Email: ${response.data.username}`) : null;
+        response.data.password ? errors.push(`Senha: ${response.data.password}`) : null;
+        response.data.detail ? errors.push(`Erro: ${response.data.detail}`) : null;
+        console.log(errors);
+        this.setState({ errors });
+      }
+    } catch (error) {
+      console.error('Erro na requisição:', error);
+    }
+  } 
+
   render() {
     return (
-      <SafeAreaView style={styles.container}>
-        <Text style={styles.title}>Faça seu Login</Text>
+      <ImageBackground source={require('../../assets/TeenPod-AuthPage.png')} style={styles.container}>
+        <Text style={styles.title}>
+          {this.state.stageNew ? 'Crie sua conta' : 'Faça seu Login'}
+        </Text>
         <TextInput
-          placeholder='Insira seu Email aqui'
+          placeholder='Nome de Usuário, sem espaços, somente letra minuscula sem acento'
           style={styles.input}
           autoFocus={true}
-          keyboardType='email-address'
-          value={this.state.email}
-          placeholderTextColor='#c0c0c0'
-          onChangeText={email => this.setState({ email })}
+          value={this.state.username}
+          placeholderTextColor='#cecece'
+          onChangeText={username => this.setState({ username })}
         />
+        {this.state.stageNew ?
+          <>
+            <TextInput
+              placeholder='Primeiro Nome'
+              style={styles.input}
+              value={this.state.firstName}
+              placeholderTextColor='#cecece'
+              onChangeText={firstName => this.setState({ firstName })}
+            />
+            <TextInput
+              placeholder='Segundo/Ultimo Nome'
+              style={styles.input}
+              value={this.state.lastName}
+              placeholderTextColor='#cecece'
+              onChangeText={lastName => this.setState({ lastName })}
+            />
+
+          </>
+          : null}
         <TextInput
           placeholder='Insira sua Senha Aqui'
           style={styles.input}
@@ -76,6 +155,16 @@ export default class Login extends Component {
           placeholderTextColor='#c0c0c0'
           onChangeText={password => this.setState({ password })}
         />
+        {this.state.stageNew ?
+          <TextInput
+            placeholder='Confirme sua senha'
+            style={styles.input}
+            secureTextEntry={true}
+            value={this.state.confirmPassword}
+            placeholderTextColor='#c0c0c0'
+            onChangeText={confirmPassword => this.setState({ confirmPassword })}
+          />
+          : null}
         {this.state.showError ?
           <View style={styles.buttomError}>
             <FlatList
@@ -84,15 +173,29 @@ export default class Login extends Component {
               renderItem={({ item }) => {
                 return <Text style={styles.buttomTextError}><Ionicons name='close' size={20} />{item}</Text>
               }} /></View> : null}
-        <TouchableOpacity onPress={this.login} style={styles.buttom}>
-          <Text style={styles.buttomText}>Entrar  <Ionicons name='send' size={20} color='#fff' /></Text>
+        <TouchableOpacity onPress={this.signinOrSignup} style={styles.buttom}>
+          <Text style={styles.buttomText}>{this.state.stageNew ? 'Registrar' : 'Entrar'}  </Text>
+          <Ionicons name='log-in-outline' size={25} color='#fff' />
         </TouchableOpacity>
         <TouchableOpacity
-          onPress={() => Linking.openURL('https://academytaskhub.pythonanywhere.com/auth/register/')}
-          style={[styles.buttom, { backgroundColor: '#AF08D9'}]} >
-          <Text style={styles.buttomText}>Criar nova conta ...</Text>
+          onPress={() => {
+            this.setState({ stageNew: !this.state.stageNew })
+          }}
+
+          style={{
+            marginTop: 20,
+            width: '80%',
+            alignItems: 'center',
+            flexDirection: 'row'
+          }} >
+          <Text style={{
+            fontSize: 15,
+            color: '#c0c0c0',
+          }}>
+            {this.state.stageNew ? 'Já possui uma conta? Entrar' : 'Não possui conta? Criar'}
+          </Text>
         </TouchableOpacity>
-      </SafeAreaView>
+      </ImageBackground>
     )
   }
 }
@@ -117,7 +220,8 @@ const styles = StyleSheet.create({
     width: '80%',
     borderRadius: 10,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    flexDirection: 'row'
   },
   buttomError: {
     marginTop: 10,
@@ -128,6 +232,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     justifyContent: 'center',
     alignItems: 'center',
+    textAlign: 'center'
   },
   buttomTextError: {
     fontSize: 10,
@@ -138,9 +243,9 @@ const styles = StyleSheet.create({
     color: '#fff',
   },
   input: {
-    marginTop: 20,
+    marginTop: 10,
     width: '80%',
-    backgroundColor: '#111',
+    backgroundColor: '#555',
     height: 40,
     borderWidth: 1,
     borderColor: '#333',
